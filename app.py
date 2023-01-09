4795b8d35991c3b919ad2ee2255ef390b6df629d
SoumakChakraborty <chakrabortysoumak11@gmail.com>
2023-01-09 16:18:37 +0530

    Threading added

--- app.py
+++ app.py
@@ -4,7 +4,32 @@ import tensorflow as tf
 from sentence_transformers import SentenceTransformer
 from scipy.spatial.distance import cosine
 from flask import Flask,request,app,jsonify,render_template
+import PyPDF2
+import os
+import shutil
+import re
+from pathlib import Path
+import threading
 app=Flask(__name__)
+def extract_text_pdf(pth,file_name,q):
+    #print(pth,file_name)
+    file=pth+'\\'+file_name
+    #print(file)
+    pdf=PyPDF2.PdfReader(file)
+    pg=pdf.getPage(0)
+    ss=pg.extract_text()
+    '''ss=ss.lower()
+    a=ss.find('abstract')
+    if ss.find('index terms')!=-1:
+        b=ss.find('index terms')
+    else:
+        b=ss.find('keywords')
+    extracted_text=ss[a:b]'''
+    extracted_text=re.sub('\n',' ',ss)
+    extracted_text=re.sub('[^A-Za-z0-9,.;]',' ',extracted_text)
+    extracted_text=re.sub('\s+',' ',extracted_text)
+    q.append(extracted_text)
+    #return extracted_text
 
 @app.route('/',methods=['GET'])
 def home():
@@ -15,10 +40,32 @@ def predict():
         return render_template('home.html')
     model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
     model_t=tf.keras.models.load_model('./best_model/check')
-    data1=request.form.get('data1')
-    data2=request.form.get('data2')
-    ttx1=[data1]
-    ttx2=[data2]
+    #data1=request.form.get('data1')
+    #data2=request.form.get('data2')
+    print(request.files)
+    data1=request.files['data1']
+    data2=request.files['data2']
+    if Path('./temp_data').exists()==False:
+        os.mkdir('./temp_data')
+    data1.save('./temp_data/f1.csv')
+    data2.save('./temp_data/f2.csv')
+    q=[]
+    th1=threading.Thread(target=extract_text_pdf,args=('./temp_data','f1.csv',q))
+    th2=threading.Thread(target=extract_text_pdf,args=('./temp_data','f2.csv',q))
+    #t1=extract_text_pdf('./temp_data','f1.csv')
+    #t2=extract_text_pdf('./temp_data','f2.csv')
+    th1.start()
+    th2.start()
+    th1.join()
+    th2.join()
+    #ttx1=[t1]
+    #ttx2=[t2]
+    ttx1=[q[0]]
+    ttx2=[q[1]]
+    #print(ex_text)
+    #ttx1=ex_text1[0]
+    #ttx2=ex_text2[0]
+    shutil.rmtree('./temp_data')
     emb1=model.encode(ttx1)
     emb2=model.encode(ttx2)
     emb1=emb1[0]
@@ -30,7 +77,12 @@ def predict():
     er=np.append(er,d)
     er=er.reshape(1,er.shape[0],1)
     res=np.where(model_t.predict(er)[0][0]>=.5,1,0)
-    return render_template('home.html',prediction="Prediction:{}".format(str(res)))
+    outp=''
+    if res==1:
+        outp='Same document with probability {}'.format(str(model_t.predict(er)[0][0]))
+    else:
+        outp='Different document with probability {}'.format(str(1-model_t.predict(er)[0][0]))
+    return render_template('home.html',prediction="Prediction:{}".format(outp))
 
 if __name__=='__main__':
     app.run(debug=True)
